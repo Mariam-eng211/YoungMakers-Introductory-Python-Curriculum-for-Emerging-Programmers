4f320dd514326111eeb9e6cfa3539350b904b627
Mariamm <manamamatova@gmail.com>
2026-07-26 18:10:16 +0600

    Add weather-based clothing advice script

---
+++ exercises/week-2-practice3.py
@@ -0,0 +1,10 @@
+weather = input('What is the weather: ')
+
+if weather == "sunny":
+    print("Wear sunglasses! 🕶️")
+elif weather == "rainy":
+    print("Grab an umbrella! ☔")
+elif weather == "snowy":
+    print("Put on a winter coat! 🧥")
+else:
+    print("Just wear a comfy hoodie! 😊")
