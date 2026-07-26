66706bfe9b5bc94734c3221aeb5702971c34b54e
Mariamm <manamamatova@gmail.com>
2026-07-26 18:08:50 +0600

    Add interactive door choice game

---
+++ exercises/week-2-practice2.py
@@ -0,0 +1,12 @@
+print("You are standing in front of a spooky castle!") 
+print("You see two doors:") 
+print("1 - The Creepy Wooden Door ") 
+print("2 - The Shiny Golden Door ") 
+# The class votes!
+ choice = input("Which door do you open? (Type 1 or 2): ")
+ if choice == "1": 
+    print("Oops! A friendly zombie 🧟 jumps out and steals your shoes!") 
+elif choice == "2": 
+  print("Jackpot! You found a chest full of pizza 🍕 and gold!") 
+else: 
+  print("You stood still for too long and a spider 🕷️ crawled on your head!")
