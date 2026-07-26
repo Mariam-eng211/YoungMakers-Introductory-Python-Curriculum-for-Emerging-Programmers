8dcc2c1c6360572f9ca64d1f561551e70fc3ece7
Mariamm <manamamatova@gmail.com>
2026-07-26 15:17:20 +0600

    Add user input for game details in week-1-practice.py

---
+++ exercises/week-1-practice.py
@@ -0,0 +1,5 @@
+game=input('what is the game your playing right now?:')
+play=input('who are you playing with? ')
+time=input('when are you going to stop playing?')
+print('Thats also my favourite game! Is it called '+game+' I also like playing with my '+play+'I also stop playing at '+time)
+MORE EXERCICES IN MY VIDEO! at /README.md
