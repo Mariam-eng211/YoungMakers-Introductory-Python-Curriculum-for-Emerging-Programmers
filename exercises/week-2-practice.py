272631fabd41f0eabdd48ba0a6a9ba1ac727341b
Mariamm <manamamatova@gmail.com>
2026-07-26 18:07:03 +0600

    Create week-2-practice.py

---
+++ exercises/week-2-practice.py
@@ -0,0 +1,8 @@
+age=int(input(‘Enter your age: :))
+
+if age >= 10:
+    print('Your’e a teenager or adult')
+elif age <= 10:
+    print("Your’e a baby or a child")
+else:
+    print("Incorrect age")
