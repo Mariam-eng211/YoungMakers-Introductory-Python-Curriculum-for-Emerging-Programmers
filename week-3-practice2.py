bd2c3453a5d0af7a4f35f00b6e1ce2d6b9f41e56
Mariamm <manamamatova@gmail.com>
2026-08-02 20:02:43 +0600

    Add temperature check for warm or cool days

---
+++ week-3-practice2.py
@@ -0,0 +1,5 @@
+temperature = float(input("Enter the temperature in °C: "))
+ if temperature > 25:
+        print("It's a warm day, dress light!")
+ else: 
+        print("It's cool outside, grab a jacket.")
