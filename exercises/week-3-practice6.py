775c2357625bfcc7a7af159079c01261e2f62b4c
Mariamm <manamamatova@gmail.com>
2026-08-02 20:06:52 +0600

    Add for loop to classify numbers as even or odd

---
+++ exercises/week-3-practice6.py
@@ -0,0 +1,9 @@
+#Combining for Loops with if / else
+
+numbers = [12, 5, 8, 21, 3, 14]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(f"{num} is Even")
+    else:
+        print(f"{num} is Odd")
