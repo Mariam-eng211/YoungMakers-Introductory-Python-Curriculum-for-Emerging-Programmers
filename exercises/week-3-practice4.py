686b4d1d8ccb3fe97ad2fcb5235cb48f70eec100
Mariamm <manamamatova@gmail.com>
2026-08-02 20:04:53 +0600

    Implement loop to print messages and range explanations
    Added a loop to print iteration numbers and explanations of range() usage.

---
+++ exercises/week-3-practice4.py
@@ -0,0 +1,8 @@
+# Print a message 5 times (counts from 0 to 4)
+for i in range(5):
+    print(f"Loop iteration number: {i}")
+
+#How range() works:
+#range(5) generates numbers 0, 1, 2, 3, 4 (starts at 0 by default, stops before 5).
+#range(1, 6) generates numbers 1, 2, 3, 4, 5 (start, stop).
+#range(2, 11, 2) generates 2, 4, 6, 8, 10 (start, stop, step size).
